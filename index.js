c8225ba7763e0c11ab0ac14c51a898ffda1e1c9f
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-02-24 00:37:57 -0600

    commit tree a2d6c2a3eb205949cac8f0f1323e799240e89b0e

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(d => {return d.toLowerCase() === name.toLowerCase()});
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(d => {return d.startsWith(string)});
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(d => {return d.name.toLowerCase() === string.toLowerCase()}); 
+}
